5004bbdf9007bc5dee5385e557fef014696447c2
ItsMeJules <jules.prn06@gmail.com>
2023-07-20 19:43:03 +0200

    Started working on the homepage

---
+++ front/src/homepage/Homepage.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+import "./Homepage.scss"
+
+export default class HomePage extends React.Component {
+
+	queueMatchmaking() {
+
+	}
+
+	displayLeaderboard() {
+
+	}
+
+	openChat() {
+
+	}
+
+	displayHistory() {
+
+	}
+
+	profilePage() {
+
+	}
+
+	friendsPage() {
+
+	}
+
+	logout() {
+
+	}
+
+	render() {
+		return (
+			<div className="home-page">
+				<h1 className="heading">THE REAL PONG</h1>
+	  
+				<div className="center-menu">
+					<p onClick={this.queueMatchmaking()}>PLAY</p>
+					<p onClick={this.displayLeaderboard()}>LEADERBOARD</p>
+					<p onClick={this.openChat()}>CHAT</p>
+					<p onClick={this.displayHistory()}>HISTORY</p>
+				</div>
+
+				<div className="bottom-left-menu">
+					<p onClick={this.profilePage()}>PROFILE</p>
+					<p onClick={this.friendsPage()}>FRIENDS</p>
+					<p onClick={this.logout()}>LOGOUT</p>
+				</div>
+			</div>
+		)
+	}
+}
--- front/src/index.js
+++ front/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.scss';
-import Signin from './login/signin/Signin.jsx';
+
+// import Signin from './login/signin/Signin.jsx';
+import Homepage from './homepage/Homepage.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Signin />
+    <Homepage />
   </React.StrictMode>
 );
